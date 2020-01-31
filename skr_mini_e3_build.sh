cd5eeff67903932398df31acdea260c5bbbfe2cd
Ruairi O'Donnell <lapido@gmail.com>
2020-01-31 16:53:48 +0100

    Updated to include Reddit guide changes

--- skr_mini_e3_build.sh
+++ skr_mini_e3_build.sh
@@ -14,7 +14,7 @@ ${VENV_DIR}/penv/Scripts/python -m venv ${VENV_DIR}
 ${VENV_DIR}/penv/Scripts/pip install -U platformio --no-cache-dir
 
 
-//git clone https://github.com/MarlinFirmware/Marlin ${MARLIN_DIR}
+#git clone https://github.com/MarlinFirmware/Marlin ${MARLIN_DIR}
 cd Marlin
 git add .
 git commit -m "Updates"
@@ -24,8 +24,6 @@ git checkout -b upstream/bugfix-2.0.x
 cd ..
 
 git -C ${MARLIN_DIR} log -1
-
-#git -C ${MARLIN_DIR} revert --no-edit 1c9ccce5209cd1727bf80e632f4f781c651e0c35
 
 
 
@@ -48,30 +46,25 @@ cp "${MARLIN_DIR}/config/examples/BigTreeTech/SKR Mini E3 1.2/Configuration_adv.
 git -C ${MARLIN_DIR} commit -a -m "base example config"
 
 sed -i 's@#define SERIAL_PORT .*@#define SERIAL_PORT 2@' ${MARLIN_DIR}/Marlin/Configuration.h
-
 sed -i 's@/*#define SERIAL_PORT_2 .*@#define SERIAL_PORT_2 -1@' ${MARLIN_DIR}/Marlin/Configuration.h
-
 sed -i 's@#define BAUDRATE .*@#define BAUDRATE 115200@' ${MARLIN_DIR}/Marlin/Configuration.h
-
 sed -i 's@ *#define MOTHERBOARD .*@  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V1_2@' ${MARLIN_DIR}/Marlin/Configuration.h
-
 sed -i 's@/*#define X_DRIVER_TYPE .*@#define X_DRIVER_TYPE  TMC2209@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@/*#define Y_DRIVER_TYPE .*@#define Y_DRIVER_TYPE  TMC2209@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@/*#define Z_DRIVER_TYPE .*@#define Z_DRIVER_TYPE  TMC2209@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@/*#define E0_DRIVER_TYPE .*@#define E0_DRIVER_TYPE TMC2209@' ${MARLIN_DIR}/Marlin/Configuration.h
-
 sed -i 's@.*#define CR10_STOCKDISPLAY@#define CR10_STOCKDISPLAY@' ${MARLIN_DIR}/Marlin/Configuration.h
-
 sed -i 's@.*#define SPEAKER@//#define SPEAKER@' ${MARLIN_DIR}/Marlin/Configuration.h
-
 sed -i 's@.*#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }.*@#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 105.68 }@' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@#define DEFAULT_MAX_FEEDRATE.*@#define DEFAULT_MAX_FEEDRATE          { 500, 500, 20, 60 }@' ${MARLIN_DIR}/Marlin/Configuration.h
+
 
 # discovered from BigTreeTech reference firmware sources
 sed -i 's@#if HAS_TMC220x && !defined(TARGET_LPC1768) && ENABLED(ENDSTOP_INTERRUPTS_FEATURE)@& \&\& !defined(TARGET_STM32F1)@g' ${MARLIN_DIR}/Marlin/src/inc/SanityCheck.h
 sed -i 's@/*#define ENDSTOP_INTERRUPTS_FEATURE@#define ENDSTOP_INTERRUPTS_FEATURE@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN@//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@/*#define FAN_SOFT_PWM@#define FAN_SOFT_PWM@' ${MARLIN_DIR}/Marlin/Configuration.h
-
+sed -i 's@/*#define SOFT_ENDSTOPS_MENU_ITEM@#define SOFT_ENDSTOPS_MENU_ITEM@' ${MARLIN_DIR}/Marlin/Configuration.h
 
 
 # beware https://github.com/MarlinFirmware/Marlin/pull/16143
@@ -90,7 +83,7 @@ sed -i 's@#define CUSTOM_MACHINE_NAME .*@#define CUSTOM_MACHINE_NAME "Lapido"@' 
 sed -i 's@.*#define SHOW_BOOTSCREEN@//#define SHOW_BOOTSCREEN@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@.*#define SHOW_CUSTOM_BOOTSCREEN@//#define SHOW_CUSTOM_BOOTSCREEN@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@.*#define CUSTOM_STATUS_SCREEN_IMAGE@//#define CUSTOM_STATUS_SCREEN_IMAGE@' ${MARLIN_DIR}/Marlin/Configuration.h
-
+#sed -i 's@/.*#define CLASSIC_JERK@#define CLASSIC_JERK@g' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@.*#define LEVEL_BED_CORNERS@#define LEVEL_BED_CORNERS@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@.*#define LEVEL_CORNERS_INSET .*@  #define LEVEL_CORNERS_INSET 33@g' ${MARLIN_DIR}/Marlin/Configuration.h
 
@@ -99,6 +92,8 @@ sed -i 's@.*#define JUNCTION_DEVIATION_MM .*@  #define JUNCTION_DEVIATION_MM 0.0
 #sed -i 's@.*#define S_CURVE_ACCELERATION@//#define S_CURVE_ACCELERATION@g' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@.*#define LIN_ADVANCE@#define LIN_ADVANCE@g' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 sed -i 's@.*#define LIN_ADVANCE_K .*@  #define LIN_ADVANCE_K 0.50@g' ${MARLIN_DIR}/Marlin/Configuration_adv.h
+sed -i 's@/*#define MONITOR_DRIVER_STATUS@#define MONITOR_DRIVER_STATUS@g' ${MARLIN_DIR}/Marlin/Configuration_adv.h
+sed -i 's@/*#define CANCEL_OBJECTS@#define CANCEL_OBJECTSg' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 
 sed -i 's@/.*#define Z_MIN_PROBE_REPEATABILITY_TEST@  #define Z_MIN_PROBE_REPEATABILITY_TEST@g' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@/.*#define RESTORE_LEVELING_AFTER_G28@  #define RESTORE_LEVELING_AFTER_G28@g' ${MARLIN_DIR}/Marlin/Configuration.h
@@ -109,10 +104,10 @@ sed -i 's@/.*#define NOZZLE_CLEAN_FEATURE@  #define NOZZLE_CLEAN_FEATURE@g' ${MA
 
 sed -i 's@.*#define ENDSTOPS_ALWAYS_ON_DEFAULT@#define ENDSTOPS_ALWAYS_ON_DEFAULT@g' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 
-sed -i 's@.*#define INDIVIDUAL_AXIS_HOMING_MENU@//#define INDIVIDUAL_AXIS_HOMING_MENU@' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@.*#define INDIVIDUAL_AXIS_HOMING_MENU@#define INDIVIDUAL_AXIS_HOMING_MENU@' ${MARLIN_DIR}/Marlin/Configuration.h
 
 sed -i 's@.*#define SQUARE_WAVE_STEPPING@  //#define SQUARE_WAVE_STEPPING@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
-
+sed -i 's@/.*#define PRINT_PROGRESS_SHOW_DECIMALS@#define PRINT_PROGRESS_SHOW_DECIMALS@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 
 
 # tmc stepper driver hybrid stealthchop/spreadcycle
@@ -138,12 +133,12 @@ sed -i 's@.*#define LCD_SET_PROGRESS_MANUALLY@#define LCD_SET_PROGRESS_MANUALLY@
 sed -i 's@.*#define SHOW_REMAINING_TIME@  #define SHOW_REMAINING_TIME@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 sed -i 's@.*#define USE_M73_REMAINING_TIME@    #define USE_M73_REMAINING_TIME@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 sed -i 's@.*#define ROTATE_PROGRESS_DISPLAY@    #define ROTATE_PROGRESS_DISPLAY@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
-
+sed -i 's@/.*#define STATUS_HEAT_PERCENT@#define STATUS_HEAT_PERCENT@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 
 
 # nozzle parking
 sed -i 's@.*#define NOZZLE_PARK_FEATURE@#define NOZZLE_PARK_FEATURE@' ${MARLIN_DIR}/Marlin/Configuration.h
-sed -i 's@.*#define NOZZLE_PARK_POINT .*@  #define NOZZLE_PARK_POINT { 5, 175, 100 }@' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@.*#define NOZZLE_PARK_POINT .*@  #define NOZZLE_PARK_POINT { 0, (Y_MAX_POS - 10), 20 }@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@.*#define EVENT_GCODE_SD_STOP .*@  #define EVENT_GCODE_SD_STOP "G1 E-3 F3600\\nG27 P2"@g' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 
 
@@ -177,14 +172,14 @@ sed -i 's@.*runout.enabled = true@    runout.enabled = false@g' ${MARLIN_DIR}/Ma
 
 
 # hot-end pid retuning
-sed -i 's@#define DEFAULT_Kp 21.73@#define DEFAULT_Kp 23.03@' ${MARLIN_DIR}/Marlin/Configuration.h
-sed -i 's@#define DEFAULT_Ki 1.54@#define DEFAULT_Ki 1.68@' ${MARLIN_DIR}/Marlin/Configuration.h
-sed -i 's@#define DEFAULT_Kd 76.55@#define DEFAULT_Kd 78.91@' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@#define DEFAULT_Kp 21.73@#define DEFAULT_Kp 23.24@' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@#define DEFAULT_Ki 1.54@#define DEFAULT_Ki 2.03@' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@#define DEFAULT_Kd 76.55@#define DEFAULT_Kd 66.60@' ${MARLIN_DIR}/Marlin/Configuration.h
 
 
 
 # make sure bed pid temp remains disabled, to keep compatibility with flex-steel pei
-sed -i 's@.*#define PIDTEMPBED@//#define PIDTEMPBED@' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@.*#define PIDTEMPBED@#define PIDTEMPBED@' ${MARLIN_DIR}/Marlin/Configuration.h
 
 
 
@@ -219,12 +214,17 @@ sed -i 's@/*#define LCD_BED_LEVELING@#define LCD_BED_LEVELING@' ${MARLIN_DIR}/Ma
 sed -i 's@/*#define AUTO_BED_LEVELING_BILINEAR@#define AUTO_BED_LEVELING_BILINEAR@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@.*#define GRID_MAX_POINTS_X .*@  #define GRID_MAX_POINTS_X 3@' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@/*#define NOZZLE_TO_PROBE_OFFSET .*@#define NOZZLE_TO_PROBE_OFFSET { -60, -12, -2.55 }@' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@#define XY_PROBE_SPEED .*@#define XY_PROBE_SPEED 6000@' ${MARLIN_DIR}/Marlin/Configuration.h
+
+
 sed -i 's@/*#define MIN_PROBE_EDGE .*@#define MIN_PROBE_EDGE 60@g' ${MARLIN_DIR}/Marlin/Configuration.h
 sed -i 's@/*#define EXTRAPOLATE_BEYOND_GRID@#define EXTRAPOLATE_BEYOND_GRID@g' ${MARLIN_DIR}/Marlin/Configuration.h
-sed -i 's@.*#define BABYSTEP_MULTIPLICATOR_Z .*@  #define BABYSTEP_MULTIPLICATOR_Z 5@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
+sed -i 's@.*#define BABYSTEP_MULTIPLICATOR_Z .*@  #define BABYSTEP_MULTIPLICATOR_Z 8@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 sed -i 's@.*#define BABYSTEP_MULTIPLICATOR_XY .*@  #define BABYSTEP_MULTIPLICATOR_XY 5@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
-
-
+sed -i 's@/*#define BLTOUCH_DELAY 500@#define BLTOUCH_DELAY 500@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
+sed -i 's@/*#define BABYSTEP_DISPLAY_TOTAL@#define BABYSTEP_DISPLAY_TOTAL@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
+sed -i 's@/*#define BABYSTEP_ZPROBE_OFFSET@#define BABYSTEP_ZPROBE_OFFSET@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
+sed -i 's@/*#define BABYSTEP_ZPROBE_GFX_OVERLAY@#define BABYSTEP_ZPROBE_GFX_OVERLAY@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 
 # bltouch probe as z-endstop on z-endstop connector
 sed -i 's@#define Z_MAX_POS .*@#define Z_MAX_POS 235@g' ${MARLIN_DIR}/Marlin/Configuration.h
@@ -241,8 +241,8 @@ sed -i 's@.*#define Z_STOP_PIN.*@#define Z_STOP_PIN         PC14@g' ${MARLIN_DIR
 
 # debugging
 #sed -i 's@/*#define MIN_SOFTWARE_ENDSTOP_Z@//#define MIN_SOFTWARE_ENDSTOP_Z@' ${MARLIN_DIR}/Marlin/Configuration.h
-#sed -i 's@/*#define DEBUG_LEVELING_FEATURE@#define DEBUG_LEVELING_FEATURE@g' ${MARLIN_DIR}/Marlin/Configuration.h
-#sed -i 's@.*#define TMC_DEBUG@  #define TMC_DEBUG@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
+sed -i 's@/*#define DEBUG_LEVELING_FEATURE@#define DEBUG_LEVELING_FEATURE@g' ${MARLIN_DIR}/Marlin/Configuration.h
+sed -i 's@.*#define TMC_DEBUG@  #define TMC_DEBUG@' ${MARLIN_DIR}/Marlin/Configuration_adv.h
 
 
 
